2e8f188b47727c1d5a90badbb7ebe635ca6e73d2
bernecg <bernard.caye@gmail.com>
2020-04-27 23:47:58 -0300

    Fix things to heroku

--- src/main/java/com/bernardcg/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/bernardcg/conferencedemo/config/PersistenceConfiguration.java
@@ -9,17 +9,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(System.getenv("DB_URL"));
-        String username = System.getenv("DB_USERNAME");
-        String password = System.getenv("DB_PASSWORD");
-        if (!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
-            builder.username(username);
-            builder.password(password);
-        }
-        System.out.println("My custom datasource bean has been initialized and set...");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() throws URISyntaxException {
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = System.getenv("JDBC_DATABASE_USERNAME");
+//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url(dbUrl);
+//        builder.username(username);
+//        builder.password(password);
+//
+//        return builder.build();
+//    }
 }
